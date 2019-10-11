e34c282643ef2d5639d352f379bff3324b307f2a
hp <2089556811@qq.com>
2019-10-11 16:08:51 +0800

    MobileSHOP_ad

---
+++ app/src/main/java/com/yzc/myfragment/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yzc.myfragment.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yzc.myfragment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
